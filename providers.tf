0061e33d9245399c4092be602fabc3d787e2afa4
Leandro <leandro.portocci@parfin.io>
2022-08-02 08:19:57 -0300

    update html

--- providers.tf
+++ providers.tf
@@ -1,3 +1,5 @@
+Terraform {
+
 # -------------------------------------------------------------*
 # Configure the Google Cloud provider
 # -------------------------------------------------------------*
@@ -6,9 +8,17 @@
 # to new major versions that may contain breaking changes, 
 # it is recommended to add version = "..." constraints to the
 # corresponding provider blocks in configuration
+
 provider "google" {
   # version = "~> 3.77.0"
   project = "aleyant-demo-358210"
   region  = "us-central1-c"
 }
-####
+
+
+provider "acme" {
+      source  = "vancluever/acme"
+      version = "~> 2.5.3"
+    }
+  
+}
